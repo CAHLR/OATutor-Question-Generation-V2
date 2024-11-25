95b13993e8d3385c6abbdb4124a6d5cb8d775c03
Shreya Bhandari <shreya.bhandari0220@gmail.com>
2024-11-25 01:41:37 -0600

    added

--- src/platform-logic/Platform.js
+++ src/platform-logic/Platform.js
@@ -513,8 +513,7 @@ class Platform extends React.Component {
                 {this.state.status === "exhausted" ? (
                     <center>
                         <h2>
-                            Thank you for learning with {SITE_NAME}. You have
-                            finished all problems.
+                        Thank you for completing the study. Your completion code is C134P6MO.
                         </h2>
                     </center>
                 ) : (
@@ -523,8 +522,7 @@ class Platform extends React.Component {
                 {this.state.status === "graduated" ? (
                     <center>
                         <h2>
-                            Thank you for learning with {SITE_NAME}. You have
-                            mastered all the skills for this session!
+                        Thank you for completing the study. Your completion code is C134P6MO.
                         </h2>
                     </center>
                 ) : (
